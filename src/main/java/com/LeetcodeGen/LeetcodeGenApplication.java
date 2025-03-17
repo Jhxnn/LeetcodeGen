feb27e23b7b516a72479e20f523fb58fc20aa5fb
Hilixx <jhonalizan@gmail.com>
2025-03-17 13:16:21 -0300

    packages

---
+++ src/main/java/com/LeetcodeGen/LeetcodeGenApplication.java
@@ -0,0 +1,13 @@
+package com.LeetcodeGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeGenApplication.class, args);
+	}
+
+}
