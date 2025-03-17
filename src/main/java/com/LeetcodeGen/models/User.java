13db1985810343b8103485b78417fec07a877c3e
Hilixx <jhonalizan@gmail.com>
2025-03-17 13:23:34 -0300

    teste

---
+++ src/main/java/com/LeetcodeGen/models/User.java
@@ -0,0 +1,14 @@
+package com.LeetcodeGen.models;
+
+
+import io.swagger.v3.oas.annotations.tags.Tags;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+
+
+}
